e19811f972aae0a809eaf5d3a28f9c18e3cef342
Jinhyuk Lee <eces@mstock.org>
2013-05-26 22:32:58 +0900

    Connect mintpresso

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -2,9 +2,16 @@ package controllers
 
 import play.api._
 import play.api.mvc._
+import play.api.Play.current
+import com.mintpresso._
 
 object Application extends Controller {
   
+  val mintpresso: Affogato = Affogato( 
+    Play.configuration.getString("mintpresso.api").getOrElse(""),
+    Play.configuration.getLong("mintpresso.id").getOrElse(0L)
+  )
+
   def index = Action {
     Ok(views.html.index("Your new application is ready."))
   }
