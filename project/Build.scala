e044590aa4e49038b135ccbf0b26acdacbbd6cc4
Jinhyuk Lee <eces@mstock.org>
2013-05-26 21:02:12 +0900

    Add dependency of mintpresso affogato scala api

--- project/Build.scala
+++ project/Build.scala
@@ -8,14 +8,14 @@ object ApplicationBuild extends Build {
   val appVersion      = "1.0-SNAPSHOT"
 
   val appDependencies = Seq(
-    // Add your project dependencies here,
-    jdbc,
-    anorm
+    "com.mintpresso" %% "mintpresso" % "0.1.9"
   )
 
 
   val main = play.Project(appName, appVersion, appDependencies).settings(
     // Add your own project settings here      
+  ) dependsOn (
+    RootProject(uri("git://github.com/admire93/Affogato.git"))
   )
 
 }
